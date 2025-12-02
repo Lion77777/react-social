9b73a7994f5355eb5ca192c325955da699617b78
Lion77777 <leon.avetisyan.1993@mail.ru>
2025-12-02 10:37:08 +0400

    Implement Slider logic

--- src/s2-homeworks/hw11/HW11.tsx
+++ src/s2-homeworks/hw11/HW11.tsx
@@ -17,6 +17,13 @@ function HW11() {
 
     const change = (event: any, value: any) => {
         // пишет студент // если пришёл массив - сохранить значения в оба useState, иначе в первый
+
+        if(Array.isArray(value)) {
+            setValue1(event.target.value[0])
+            setValue2(event.target.value[1])
+        } else {
+            setValue1(event.target.value)
+        }
     }
 
     return (
@@ -30,7 +37,8 @@ function HW11() {
                         <SuperRange
                             id={'hw11-single-slider'}
                             // сделать так чтоб value1 изменялось // пишет студент
-
+                            onChange={(e) => change(e, value1)}
+                            value={value1}
                         />
                     </div>
                     <div className={s.wrapper}>
@@ -38,7 +46,8 @@ function HW11() {
                         <SuperRange
                             id={'hw11-double-slider'}
                             // сделать так чтоб value1/2 изменялось // пишет студент
-
+                            onChange={(e) => change(e, [value1, value2])}
+                            value={[value1, value2]}
                         />
                         <span id={'hw11-value-2'} className={s.number}>{value2}</span>
                     </div>
